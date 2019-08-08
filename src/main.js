b4fbfb688940436c1048ca82072589d2134039aa
Artyom Fayzulin <43291819+afayzulin@users.noreply.github.com>
2019-08-08 12:31:31 +0300

    init main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import ComponentCustomize  from './ComponentCustomize.js'
+Vue.use(ComponentCustomize, {
+    name: 'wrapper',
+});
+
+import store from '../store'
+new Vue({
+  el: '#root',
+  store
+})
